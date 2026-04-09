382c459e8c5befec8c37a21050538307da214231
sadsodach <gimmesoda.dev@gmail.com>
2026-04-09 23:57:50 +1100

    remove js condition

--- lib/discord/Api.hx
+++ lib/discord/Api.hx
@@ -3,7 +3,7 @@ package discord;
 private typedef Native = haxe.macro.MacroType<[
 	idl.Module.build({
 		idlFile: "discord.idl",
-		target: #if hl "hl" #elseif js "js" #end,
+		target: "hl",
 		packageName: "discord",
 		autoGC: true,
 		nativeLib: "discord",
